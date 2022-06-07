9cabddf8715f806c03936b7aadee918b6c29f808
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-06-07 08:39:48 -0300

    Criando projeto

---
+++ AppEstilo.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgb(3, 188, 245)'        
+    },
+});
+
+export default styles;
---
+++ components/Titulo/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: '#FFF',
+    },
+    texto:{
+        fontSize: 32
+    }
+});
+
+export default styles;
