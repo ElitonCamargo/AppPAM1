132629869f015b033df382edb3c473d4e2c0eb69
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2022-06-14 08:45:02 -0300

    Estilizando card

--- components/Card/index.js
+++ components/Card/index.js
@@ -4,13 +4,13 @@ import styles from './styles';
 
 export default function Card(){
     return(
-        <View>
-            <View>
+        <View style={styles.container}>
+            <View style={styles.card}>
                 <Image
                     source={{uri:'https://marvelucm.com/wp-content/uploads/2022/03/Morbius-Jared-Leto-1.jpg'}}
                     style={styles.img}
                 />
-                <Text>
+                <Text style={styles.texto}>
                     Após relançamento nos cinemas, 'Morbius' FRACASSA novamente nas bilheterias
                 </Text>
             </View>
